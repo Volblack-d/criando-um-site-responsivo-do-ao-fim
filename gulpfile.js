5ebb5125a6f734d53da9897e4ee546f7d9146295
Volblack-d <volblack@gmail.com>
2017-09-24 18:35:00 -0300

    adicionado arquivos branch html

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
-//task para o sass
+// task para o sass
 gulp.task('sass', function() {
-   return gulp.src('sass/**/*.sass')
+	return gulp.src('sass/**/*.sass')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError)) 
         .pipe(gulp.dest('css'));
 });
-//task para watch
-gulp.task('watch', function() {
+
+// task para watch
+gulp.task('watch', function(){
     gulp.watch('sass/**/*.sass', ['sass']);
 });
 
-//task defaukt gulp
+// task default gulp
 gulp.task('default', ['sass', 'watch']);
